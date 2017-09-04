369f030e36f6f2ddd81d3e63511c88b35234e13f
art32fil <art32fil@gmail.com>
2017-09-04 11:24:33 +0300

    useful scripts

---
+++ lab1/is_correct_package.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+i=0
+packages=""
+for arg in $@; do
+	if (( i == 0 )); then
+		current_dir=$arg/src
+	else
+		packages=" $arg"
+	fi
+	let "i=i+1"
+done
+
+if [[ $packages == "" ]] || [[ $packages == " " ]]; then
+	(>&2 echo -e "Error: no packages are matched to be recognized")
+	exit 1
+fi
+
+for package in $packages; do
+	package=${package##*/}
+	if [[ $(find $current_dir -maxdepth 1 -mindepth 1 -name "$package" )
+	xml=$(find $current_dir/$package -maxdepth 1 -mindepth 1 -name "package.xml")
+	if [[ "$xml" == "" ]]; then
+		(>&2 echo -e "Error: no package.xml file in a package directory")
+		(>&2 echo -e "       package directory: $current_dir/$package")
+		exit 1
+	fi
+	project_xml=$(cat $xml | grep "<name>")
+	project_xml=${project_xml#*<name>}
+	project_xml=${project_xml%</name>*}
+
+	cmake=$(find $current_dir/$package -maxdepth 1 -mindepth 1 -name "CMakeLists.txt")
+	if [[ "$cmake" == "" ]]; then
+		(>&2 echo -e "Error: no CMakeLists.txt file in a package directory")
+		(>&2 echo -e "       package directory: $current_dir/$package")
+		exit 1
+	fi
+	project_cmake=$(cat $cmake | grep "project(")
+	project_cmake=${project_cmake#*project(}
+	project_cmake=${project_cmake%%)*}
+
+	if [[ "$project_xml" != "$project_cmake" ]]; then
+		(>&2 echo -e "Error: project name is not defined")
+		(>&2 echo -e "       package.xml contains:    $project_xml")
+		(>&2 echo -e "       CMakeLists.txt contains: $project_cmake")
+		exit 1
+	fi
+	echo -e "package $package is correct!"
+done
+exit 0
---
+++ lab1/is_ros_workspace.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+length() {
+	echo "$#"
+}
+
+current_dir=$1
+
+#find catkin_workspace folder in the requested directory
+dirs=$(find $current_dir -maxdepth 1 -mindepth 1 -type d -name "[^.]*" | tr '\n' ' ')
+dirs=${dirs% }
+if [[ "$dirs" == "" ]]; then
+	(>&2 echo -e "Error: no catkin_workspace directory in the local workspace folder")
+	exit 1
+fi
+
+if [ "$(length $dirs)" -gt "1" ]; then
+	(>&2 echo -e "Error: many ($(length $dirs)) workspace directories were found: $dirs")
+	exit 1
+fi
+workspace=$current_dir/$dirs
+
+#find src folder in the worspace folder
+dirs=$(find $workspace -maxdepth 1 -mindepth 1 -type d -name "[^.]*" | tr '\n' ' ')
+dirs=${dirs% }
+if [[ "$(echo $dirs | grep src)" == "" ]]; then
+	(>&2 echo -e "Error: no src folder in the workspace ($workspace)")
+	(>&2 echo -e "       found dirs: ($dirs)")
+	exit 1
+fi
+src=$workspace"/src"
+
+#find CMakeLists.txt file
+all=$(find $src -maxdepth 1 -mindepth 1 -name "[^.]*" | tr '\n' ' ')
+if [[ "$(echo $all | grep CMakeLists.txt)" == "" ]]; then
+	(>&2 echo -e "Error: no CMakeLists.txt file in the src folder ($src)")
+	exit 1
+fi
+link=$(readlink $src/CMakeLists.txt)
+
+#make sure that set link is correct
+if [[ "$link" != "/opt/ros/kinetic/share/catkin/cmake/toplevel.cmake" ]]; then
+	(>&2 echo -e "Error: CMakelists.txt link is not correct")
+	(>&2 echo -e "       current link: $link")
+	(>&2 echo -e "       request link: /opt/ros/kinetic/share/catkin/cmake/toplevel.cmake")
+	exit 1
+fi
+
+echo -e "workspace was created succesfully"
+exit 0
