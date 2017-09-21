59bc1f3c7219d78364ce6e9ffd8aa8fe5d215c9e
art32fil <art32fil@gmail.com>
2017-09-21 16:03:40 +0300

    3 predict file correction

--- answers/publisher_subscriber/is_correct.sh
+++ answers/publisher_subscriber/is_correct.sh
@@ -6,7 +6,9 @@ got_file=$2
 if [[ $(cat $got_file | grep "/subscriber: ") != $predict_file ]]; then
 	echo -e "Error: subscriber works incorrect"
 	echo -e "       the result is:"
-	echo -e "$(cat $got_file | grep \"/subscriber: \")"
+	echo -e "$(cat $got_file | grep '/subscriber: ')"
+	echo -e "       the predict is:"
+	echo -e "$(cat $predict_file)"
 	exit 1;
 fi
 echo -e "success"
