34460f630a1fc067a1e6ce6ad7190cc27596a619
art32fil <art32fil@gmail.com>
2017-09-22 13:37:52 +0300

    3 catkin errors recognition

--- publisher_subscriber/src/subscriber/src/simple_subscriber.cpp
+++ publisher_subscriber/src/subscriber/src/simple_subscriber.cpp
@@ -2,14 +2,11 @@
 #include "std_msgs/Int32.h"
 #include <iostream>
 
-bool active = false;
-
 using std::cin;
 using std::cout;
 using std::endl;
 
 void function(const std_msgs::Int32 mes) {
-	active = true;
 	cout << ros::this_node::getName().c_str() << ": data: " << mes.data << endl;
 }
 
@@ -17,13 +14,6 @@ int main(int argc, char** argv) {
 	ros::init(argc,argv,"node");
 	ros::NodeHandle n;
 	ros::Subscriber pub = n.subscribe("/topic",1000,function);
-	int max_iters = 20;
-	for (int i = 0; i < 20; i++) {
-		if (active) {
-			ros::spin();
-			return 0;
-		}
-		sleep(1);
-	}
+	ros::spin();
 	return 0;
 }
