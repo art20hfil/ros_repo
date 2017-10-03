fcfc023b989dd6b2f1f7f31e6aed8095d39a15c0
Anton <ant.filatov@gmail.com>
2017-10-03 11:35:12 +0300

    added deleting of .py test node

--- 3/launch.sh
+++ 3/launch.sh
@@ -10,7 +10,7 @@ catkin_make -j1 >> /dev/null 2> ~/forerrors.txt
 if [[ "$(cat ~/forerrors.txt)" != "" ]]; then
 	echo -e "Error: package could not be compiled"
 	echo -e "$(cat ~/forerrors.txt)"
-        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/
+        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/ $pkg_dir/rviz_lab.py
 	exit 1
 fi
 
@@ -19,7 +19,7 @@ roscore 2>/dev/null 1>/dev/null & sleep 1 && rosrun $pkg $(ls $(catkin_find --wi
 
 if [[ $(cat ~/received_file.txt) == "" ]]; then
         echo -e "Topic /output is empty or messages cannot be recognized"
-        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/
+        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/ $pkg_dir/rviz_lab.py
         exit 1
 fi
 
@@ -31,13 +31,13 @@ if [[ $error_log != "" ]] && [[ $(cat ~/received_file.txt | grep error) == "" ]]
         echo -e "$(cat ~/answer_file.txt)"
         echo -e "You provide data:"
         echo -e "$(cat ~/received_file.txt)"
-        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/
+        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/ $pkg_dir/rviz_lab.py
         exit 1
 fi
 
 if [[ $error_log != "" ]]; then
         echo -e "Error: Your Markers are incorrect"
         echo -e "$(cat ~/received_file.txt | grep error)"
-        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/
+        rm -rf $(cat /root/workspace.txt)/build/ $(cat /root/workspace.txt)/devel/ /root/ros_repo/ $pkg_dir/rviz_lab.py
         exit 1
 fi
