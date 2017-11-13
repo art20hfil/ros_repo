5955e6e0368f1de403e1bb0b088a3d27220933b7
Anton Filatov <ant.filatov@gmail.com>
2017-11-13 10:51:46 +0300

    Update node3.cpp

--- 3/3.2/example_pkg/node3.cpp
+++ 3/3.2/example_pkg/node3.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "node3");
     ros::NodeHandle nh;
     int cost;
-    sleep(0.3);
+    sleep(2);
     std::ofstream fout("/root/out.txt", std::ios::app);
     ros::param::param<int>("~max_size", cost, -7844);
     sleep(0.5);
